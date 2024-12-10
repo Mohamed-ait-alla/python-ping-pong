9792c54efe05915c156de26b22a01bb48345505b
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2024-12-10 21:12:03 +0100

    add the ball class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddel import Paddle
+from ball import Ball
+
 screen = Screen()
 screen.title("Ping pong game")
 screen.bgcolor("black")
@@ -7,5 +9,6 @@ screen.screensize(600, 600)
 
 right_paddle = Paddle((240,0))
 left_paddle = Paddle((-240,0)) 
+ball = Ball()
 
 screen.exitonclick()
