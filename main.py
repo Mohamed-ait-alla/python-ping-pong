fe1a3be7166fcc921bc326d3f4bffdf312dba7fc
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-03-16 13:16:24 +0100

    add score feature

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
 from paddel import Paddle
 from ball import Ball
+from score import Scoreboard
 import time
 
 screen = Screen()
+
 screen.title("Ping pong game")
 screen.bgcolor("black")
 screen.screensize(600, 600)
@@ -12,8 +14,8 @@ screen.tracer(0)
 right_paddle = Paddle((240,0))
 left_paddle = Paddle((-240,0)) 
 ball = Ball()
-
-
+scoreboard = Scoreboard()
+ 
 screen.listen()
 screen.tracer(0)
 
@@ -35,10 +37,17 @@ while game_on:
     if ((ball.xcor() <= -220 and ball.distance(left_paddle) <= 50) or (ball.xcor() >= 220 and ball.distance(right_paddle) <= 50)):
         ball.x_move *= -1
         default_speed *= 0.9
-    # when it goes arround
-    if (ball.xcor() > 280 or ball.xcor() < -280):
+    # when it goes arround toward right side
+    if (ball.xcor() > 280):
         ball.goto(0, 0)
         ball.x_move *= -1
         default_speed = 0.1
+        scoreboard.update_left_score()
+    # when it goes arround toward left side
+    if (ball.xcor() < -280):
+        ball.goto(0, 0)
+        ball.x_move *= -1
+        default_speed = 0.1
+        scoreboard.update_right_score()
     
 screen.exitonclick()
---
+++ score.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("yellow")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_left_score(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def update_right_score(self):
+        self.r_score += 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(0, 200)
+        self.write("score", align="center", font=("courier", 18, "bold"))
+        self.goto(-50, 170)
+        self.write(self.l_score, align="center", font=("courier", 16, "bold"))
+        self.goto(0, 170)
+        self.color("white")
+        self.write("|", align="center", font=("courier", 18, "bold"))
+        self.color("yellow")
+        self.goto(50, 170)
+        self.write(self.r_score, align="center", font=("courier", 16, "bold"))
+    
